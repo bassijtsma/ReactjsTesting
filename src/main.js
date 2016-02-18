b5e1db6c5bc17e082ee3c0ec0f75838de2a51d13
bassijtsma <btsijtsma@gmail.com>
2016-02-18 23:22:28 +0100

    trying react router in mainjs

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Router = require('react-router')
-var routes = require('./routes')
 
+var Route = require('react-router').Route;
+var IndexRoute = require('react-router').IndexRoute;
+var hashHistory = require('react-router').hashHistory;
+
+{/* var routes = require('./routes')
 Router.run(routes, function(Handler) {
   React.render(<Handler />, document.getElementById('content'));
 })
+*/}
+
+var homePage = require('./components/home.jsx')
+var App = require('./components/App');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={homePage}>
+      <IndexRoute component={homePage}></IndexRoute>
+    </Route>
+  </Router>,
+  App
+);
