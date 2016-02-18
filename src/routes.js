752bc52481c98c37a9bddc003b199024ba10e078
bassijtsma <btsijtsma@gmail.com>
2016-02-18 23:17:26 +0100

    cleanup old react-router and start react-router 2.0

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,24 @@
 var React = require('react');
 
 var Router = require('react-router');
-var DefaultRoute = Router.DefaultRoute;
-var Route = Router.Route;
+var Route = require('react-router').Route;
+var IndexRoute = require('react-router').IndexRoute;
+var hashHistory = require('react-router').hashHistory;
 
-var routes = (
+
+var routes = React.createClass({
+  render: function(
+    return ;
+  )
+})
+
+
+{/*
+  var routes = (
   <Route name="app" path="/" handler={require('./components/app')}>
     <DefaultRoute handler={require('./components/home.jsx')} />
     <Route name="superheros" handler={require('./components/superheros/superheroPage.jsx')} />
     <Route name="about" handler={require('./components/about/aboutPage.jsx')} />
-    <Route name="test" />
   </Route>
 )
+*/}
