19d64e839b472b2ef5d1b6e9ca185d390406c5fc
bassijtsma <btsijtsma@gmail.com>
2016-02-19 21:34:35 +0100

    npm install for react router2

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,8 @@
 /*eslint-disable strict */
 $ = jQuery = require('jquery');
 var React = require('react');
-var Header = require('../common/header.jsx');
-var Footer = require('../common/footer.jsx');
+var Header = require('./common/header.jsx');
+var Footer = require('./common/footer.jsx');
 
 var App = React.createClass({
   render: function() {
